a281a73457ec43a5b32c6b3234e5b44ceffc701e
blogSoul <blo9040@gmail.com>
2020-12-03 23:14:03 +0900

    connect Detail page

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,8 +16,14 @@ export const postJoin = (req, res) => {
         res.redirect(routes.home);
     }
 };
-export const login = (req, res) => res.render("login", { pageTitle: "Login"});
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout"});
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login"});
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+export const logout = (req, res) => {
+    // todo: log out
+    res.redirect(routes.home);
+}
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "userDetail"});
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "editProfile"});
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "changePassword"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
-import { videos } from "../db"
+import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
 
@@ -10,7 +11,18 @@ export const search = (req, res) => {
     } =  req;
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-export const upload = (req, res) => res.render("upload", { pageTitle: "upload"});
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "upload"});
+export const postUpload = (req, res) => {
+    const {
+        body: {
+            file,
+            title,
+            description
+        }
+        // todo: Upload and save video
+    }= req;
+    res.redirect(routes.videoDetail(32333));
+}
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "editVideo"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "deleteVideo"});

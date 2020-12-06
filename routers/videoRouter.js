22ec8c7eaf0823e3236e14eeb865f6d49804feab
blogSoul <blo9040@gmail.com>
2020-12-06 23:07:22 +0900

    add edit funtion and delete function

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,19 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+//edit
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+//delete
+videoRouter.get(routes.deleteVideo(), deleteVideo);
+//detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;

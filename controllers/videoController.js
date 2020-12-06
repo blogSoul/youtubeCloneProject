9744055aaae12f37e6933b2ce429faa3c6786b46
blogSoul <blo9040@gmail.com>
2020-12-06 22:00:16 +0900

    add multer middleware and do Upload function

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,14 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-
-    res.render("home", { pageTitle: "Home", videos });
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({});
+        res.render("home", { pageTitle: "Home", videos });
+    } catch(error) {
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: [] });
+    }
 };
 export const search = (req, res) => {
     const {
@@ -11,16 +17,17 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "upload"});
-export const postUpload = (req, res) => {
-    const {
-        body: {
-            file,
-            title,
-            description
-        }
-        // todo: Upload and save video
+export const postUpload = async(req, res) => {
+    const { 
+        body: { title, description },
+        file: { path }
     } = req;
-    res.redirect(routes.videoDetail(32333));
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    })
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "editVideo"});

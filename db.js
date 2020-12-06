69fd200c31d486e1184cfc6ed8ac5f342a98fda9
blogSoul <blo9040@gmail.com>
2020-12-06 20:34:13 +0900

    connect db in mongodb

--- db.js
+++ db.js
@@ -1,46 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            name: "Nicolas",
-            email: "soul@gmail.com"
-        }
-    },
-    {
-        id: 3393,
-        title: 'Video home',
-        description: 'This is something I love',
-        views: 12,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            name: "Nicolas",
-            email: "soul@gmail.com"
-        }
-    },
-    {
-        id: 32333,
-        title: 'Video soul',
-        description: 'This is something I love',
-        views: 10,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            name: "Nicolas",
-            email: "soul@gmail.com"
-        }
-    },
-    {
-        id: 33,
-        title: 'Video num',
-        description: 'This is something I love',
-        views: 1,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            name: "Nicolas",
-            email: "soul@gmail.com"
-        }
-    },
-]
+        useNewUrlParser:true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ Connected to DB");
+const handleError = error => console.log(`✔ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
